9d34e4a912ee77595c7c394609c20e1928499549
Ksenia Durkina <sigismund1old@gmail.com>
2026-06-05 13:50:09 +0300

    chore: change lint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,23 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import { defineConfig } from 'eslint/config';
 import stylistic from '@stylistic/eslint-plugin';
 
-export default defineConfig([
+export default [
   {
     rules: {
-      semi: ['error', 'always'],
+      '@stylistic/semi': ['error', 'always'],
       'comma-dangle': ['error', 'always-multiline'],
-      'import/no-unresolved': 'off',
     },
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js, '@stylistic': stylistic },
-    extends: ['js/recommended'],
     languageOptions: {
       globals: { ...globals.node, ...globals.jest },
     },
   },
   {
-    files: ['eslint.config.js'], 
+    files: ['eslint.config.js'],
     rules: {
       'import/no-extraneous-dependencies': 'off',
     },
   },
-]);
+];
